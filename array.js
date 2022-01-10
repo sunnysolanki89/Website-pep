0a0f6d8d165d1048546957f8e15dbc096c942b52
sunnysolanki89 <chandrapratap.solanki@gmail.com>
2022-01-10 20:22:49 +0530

    Arrays

---
+++ array.js
@@ -0,0 +1,118 @@
+let arr = [] // array declaration
+
+let cars =['BMW','Mustang','Mercedes', 'Ferrari'] // Same data type
+//console.log(cars)
+
+// let testArr = ['BMW', 2, true, null, undefined, 20.34]
+// in JS array you can store diffrent values of diffrent DataTypes
+// console.log(testArr)
+
+// Accessing the elements of an Array
+
+// console.log(cars[2]) // Mercedes
+// console.log(cars[0]) // BMW
+// console.log(cars[3]) // Ferrari
+// console.log(cars[1]) // Mustang
+// console.log(cars[5]) // wrong index
+
+// Replacing  an Element in a array
+
+// cars[2] = 'Jaguar'
+// console.log(cars)
+
+// Add method
+
+// cars[7] = 'Bentley'
+// console.log(cars)
+
+// Array Methods
+
+// pop method - this method removes the last element of the array
+//let removedElements = cars.pop()
+//console.log(removedElements)
+
+// push method - this method adds an element to te end of an array
+//cars.push('Aston Martin', 'Maruti')
+
+// Shift and unshift
+
+// Shift method - removes an element from the starting from the array
+// shift - remove first element 
+//cars.shift()
+
+// unshift - adds an element to the 0th index (Starting of your array)
+
+// cars.unshift("Tata Nano")
+
+// length Property of an array
+let length = cars.length
+
+// 2D Array
+
+// let matrix = [
+//     [1, 2, 3, 10],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ]
+// console.log(matrix[0][3])
+
+// String methods -
+
+// 1. Extraction Methods
+let str = "pepcoders"
+// Slice method
+ 
+// Slice (start, end+1)
+
+// let slicedStr = str.slice(3,7)
+
+// console.log(slicedStr)
+// let pepdStr = str.slice(0,3)
+
+// console.log(pepdStr)
+
+//2. subStr
+
+// substr(startingIndex, lengthUpToTheLetter)
+
+// let subString = str.substr(3, 4)
+// console.log(subString)
+
+// Replacing string's content
+
+// Replace method
+
+// let sayHello ='Hello Amit'
+// console.log(sayHello)
+
+// // replace
+// let sayBye = sayHello.replace('Hello', 'Bye')
+// console.log(sayBye)
+
+// Uppercase Method and lowercase
+
+// let text1 = "Hello world"
+// text1 = text1.toUpperCase() // string fully uppercase
+// console.log(text1)
+
+// let text2 = "Hello world"
+// text2 = text2.toLowerCase() // string fully lowercase
+// console.log(text2)
+
+// Concatenation  (Concat method)
+
+// let firstString = 'Hello'
+// let secondString = 'Sunny'
+// let conCatString = firstString.concat(" ", secondString)
+// console.log(conCatString)
+
+// Trim method is used to remove all the white spaces
+
+// let text = '    Hello world     '
+// let trimmedText = text.trim()
+// console.log(trimmedText)
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,7 @@
+//console.log("Hi");
+
+//IIFE Function
+
+var IIFE =(function(a,b){
+    console.log(a*b)
+})(10 , 2)
